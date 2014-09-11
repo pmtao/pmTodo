6970cd9f03df7ee5c94907c8e0acfd8e76b75e35
阿涛 <pmtao@pmMacBook-Pro.local>
2014-09-11 16:55:15 +0800

    修改记录选中标志。 添加3个代理方法，添加代理类通知方法。

--- pmTodo/ViewController.m
+++ pmTodo/ViewController.m
@@ -30,7 +30,7 @@
     ChecklistItem *item;
     
     item = [[ChecklistItem alloc]init];
-    item.text =@"观看嫦娥⻜飞天和⽟玉兔升空的视频";
+    item.text =@"观看嫦娥⻜天和玉兔升空的视频";
     item.checked = NO;
     [_items addObject:item];
     
@@ -45,12 +45,12 @@
     [_items addObject:item];
     
     item = [[ChecklistItem alloc]init];
-    item.text =@"去电影院看地⼼心引⼒力";
+    item.text =@"去电影院看地⼼引力";
     item.checked = NO;
     [_items addObject:item];
     
     item = [[ChecklistItem alloc]init];
-    item.text =@"看⻄西甲巴萨新败的⽐比赛回放";
+    item.text =@"看西甲巴萨新败的⽐比赛回放";
     item.checked = NO;
     [_items addObject:item];
     
@@ -73,11 +73,12 @@
 //    ChecklistItem *item = _items[indexPath.row];
 
 -(void)configureCheckmarkForCell:(UITableViewCell *)cell withChecklistItem:(ChecklistItem *)item{
+    UILabel *label = (UILabel *)[cell viewWithTag:1001];
     
     if(item.checked){
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        label.text = @"√";
     }else{
-        cell.accessoryType = UITableViewCellAccessoryNone;
+        label.text = @"";
     }
     
 }
@@ -112,26 +113,60 @@
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
-- (IBAction)addItem:(id)sender {
-    NSInteger newRowIndex = [_items count];
+-(void)tableView:(UITableView *)tableView commitEditingStyle: (UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    ChecklistItem *item =[[ChecklistItem alloc]init];
-    item.text = @"我是新来的菜⻦鸟,求照顾求虐";
-    item.checked = YES;
+    [_items removeObjectAtIndex:indexPath.row];
+    NSArray *indexPaths = @[indexPath];
+    [tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+//代理方法：关闭增加记录页面
+-(void)itemDetailViewControllerDidCancel:(ItemDetailViewController *)controller{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+//代理方法：添加行并关闭增加记录页面
+-(void)itemDetailViewController:(ItemDetailViewController *)controller didFinishAddingItem:(ChecklistItem *)item{
+    NSInteger newRowIndex = [_items count];
     [_items addObject:item];
     
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:newRowIndex inSection:0];
     NSArray *indexPaths = @[indexPath];
     
     [self.tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
-    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
--(void)tableView:(UITableView *)tableView commitEditingStyle: (UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+//代理方法：编辑记录并关闭
+-(void)itemDetailViewController:(ItemDetailViewController*)controller didFinishEditingItem:(ChecklistItem*)item{
+    NSInteger index=[_items indexOfObject:item];
+    NSIndexPath *indexPath=[NSIndexPath indexPathForRow:index inSection:0];
+    UITableViewCell *cell=[self.tableView cellForRowAtIndexPath:indexPath];
+
+    [self configureTextForCell:cell withChecklistItem:item];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+//当界面跳转的时候,UIKit会触发segue的prepareForSegue方法。segue是storyboard中两个视图控制器之间的那个箭头。
+//prepareForSegue允许我们向新的视图控制器传递数据。
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     
-    [_items removeObjectAtIndex:indexPath.row];
-    NSArray *indexPaths = @[indexPath];
-    [tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+    if([segue.identifier isEqualToString:@"AddItem"]){
+        
+        UINavigationController *navigationController = segue.destinationViewController;
+        ItemDetailViewController *controller = (ItemDetailViewController*) navigationController.topViewController;
+        controller.delegate = self;
+    }else if([segue.identifier isEqualToString:@"EditItem"]){
+        
+        UINavigationController *navigationController = segue.destinationViewController;
+        ItemDetailViewController *controller = (ItemDetailViewController*) navigationController.topViewController;
+        controller.delegate = self;
+        
+        //这里的sender参数其实指的就是触发了该segue的控件,在这里就是table view cell中的细节显示按钮。
+        //通过它可以找到对应的index-path,然后获取要编辑的ChecklistItem对象的行编号。
+        NSIndexPath * indexPath = [self.tableView indexPathForCell:sender];
+        controller.itemToEdit = _items[indexPath.row];
+    }
 }
 
 @end
