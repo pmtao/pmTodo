62cc406639d55706dc92af9ba6fa09d3a50d9246
阿涛 <pmtao@pmMacBook-Pro.local>
2014-09-11 16:52:41 +0800

    申明代理类

--- pmTodo/ViewController.h
+++ pmTodo/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ItemDetailViewController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UITableViewController<ItemDetailViewControllerDelegate>
 
 @end
