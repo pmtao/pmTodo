d15125d3bc561c93c90815a105b889192fe8c0d8
阿涛 <pmtao@pmMacBook-Pro.local>
2014-09-12 11:28:43 +0800

    修改类、文件名称

---
+++ pmTodo/ChecklistViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChecklistViewController.h
+//  pmTodo
+//
+//  Created by 阿涛 on 14-9-4.
+//  Copyright (c) 2014年 PM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ItemDetailViewController.h"
+
+@interface ChecklistViewController : UITableViewController<ItemDetailViewControllerDelegate>
+
+@end
