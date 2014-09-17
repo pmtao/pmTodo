53feb79f4d6ca631cd3e14a671058cf1443bcd13
阿涛 <pmtao@pmMacBook-Pro.local>
2014-09-17 14:28:04 +0800

    修改名称

--- pmTodo/ItemDetailViewController.h
+++ pmTodo/ItemDetailViewController.h
@@ -12,7 +12,7 @@
 @class ItemDetailViewController;
 @class ChecklistItem;
 
-//ItemDetailViewController与ChecklistsViewController的代理协议，包含2个方法申明。
+//ItemDetailViewController与ChecklistViewController的代理协议，包含2个方法申明。
 @protocol ItemDetailViewControllerDelegate <NSObject>
 
 -(void)itemDetailViewControllerDidCancel:(ItemDetailViewController*)controller;
