b052d98b8877042b971fe78037a3ed9398f6c3b4
阿涛 <pmtao@pmMacBook-Pro.local>
2014-09-17 14:25:29 +0800

    增加事项类别详情视图控制器

---
+++ pmTodo/ListDetailViewController.m
@@ -0,0 +1,113 @@
+//
+//  ListDetailViewController.m
+//  pmTodo
+//
+//  Created by 阿涛 on 14-9-12.
+//  Copyright (c) 2014年 PM. All rights reserved.
+//
+
+#import "ListDetailViewController.h"
+#import "Checklist.h"
+
+@interface ListDetailViewController ()
+
+@end
+
+@implementation ListDetailViewController{
+    NSString *_iconName;
+}
+
+//- (id)initWithStyle:(UITableViewStyle)style
+//{
+//    self = [super initWithStyle:style];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+
+-(id)initWithCoder:(NSCoder *)aDecoder{
+    if((self = [super initWithCoder:aDecoder])){
+        _iconName = @"Folder";
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    if(self.checklistToEdit !=nil){
+        self.title=@"Edit Checklist";
+        self.textField.text=self.checklistToEdit.name;
+        self.doneBarButton.enabled=YES;
+        _iconName = self.checklistToEdit.iconName;
+    }
+    
+    self.iconImageView.image = [UIImage imageNamed:_iconName];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.textField becomeFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)cancel:(id)sender{
+    [self.delegate listDetailViewControllerDidCancel:self];
+}
+
+-(IBAction)done:(id)sender{
+    if(self.checklistToEdit==nil){
+        Checklist *checklist=[[Checklist alloc]init];
+        checklist.name=self.textField.text;
+        checklist.iconName = _iconName;
+        
+        [self.delegate listDetailViewController:self didFinishAddingChecklist:checklist];
+        
+    }else{
+        
+        self.checklistToEdit.name=self.textField.text;
+        self.checklistToEdit.iconName = _iconName;
+        [self.delegate listDetailViewController:self didFinishEditingChecklist:self.checklistToEdit];
+    }
+}
+
+-(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section == 1){
+        return indexPath;
+    }else{
+        return nil;
+    }
+}
+
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    NSString *newText = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    self.doneBarButton.enabled=([newText length]>0);
+    return YES;
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if([segue.identifier isEqualToString:@"PickIcon"]){
+        IconPickerViewController *controller = segue.destinationViewController;
+        controller.delegate = self;
+    }
+}
+
+//代理协议的回调方法,让程序记住所选中图标的名称。
+-(void)iconPicker:(IconPickerViewController *)picker didPickIcon:(NSString *)iconName
+{
+    _iconName = iconName;
+    self.iconImageView.image = [UIImage imageNamed:_iconName];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
