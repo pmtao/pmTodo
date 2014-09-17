bb5991ed60767651b3ea684b4fbb3eff06ffb19d
阿涛 <pmtao@pmMacBook-Pro.local>
2014-09-17 14:23:34 +0800

    增加事项类别视图控制器

---
+++ pmTodo/AllListsViewController.h
@@ -0,0 +1,18 @@
+//
+//  AllListsViewController.h
+//  pmTodo
+//
+//  Created by 阿涛 on 14-9-12.
+//  Copyright (c) 2014年 PM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ListDetailViewController.h"
+
+@class DataModel;
+
+@interface AllListsViewController : UITableViewController<ListDetailViewControllerDelegate,UINavigationControllerDelegate>
+
+@property(nonatomic,strong)DataModel *dataModel;
+
+@end
---
+++ pmTodo/AllListsViewController.m
@@ -0,0 +1,193 @@
+//
+//  AllListsViewController.m
+//  pmTodo
+//
+//  Created by 阿涛 on 14-9-12.
+//  Copyright (c) 2014年 PM. All rights reserved.
+//
+
+#import "AllListsViewController.h"
+#import "Checklist.h"
+#import "ChecklistViewController.h"
+#import "ChecklistItem.h"
+#import "DataModel.h"
+
+@interface AllListsViewController ()
+
+@end
+
+@implementation AllListsViewController
+
+#pragma mark init初始化
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+//    NSLog(@"文件夹的路径是：%@", [self documentsDirectory]);
+//    NSLog(@"数据文件的最终路径是：%@", [self dataFilePath]);
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//应用启动的时候,显示上次查看的checklist,再手动触发segue。
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    self.navigationController.delegate = self;
+    
+    NSInteger index=[self.dataModel indexOfSelectedChecklist];
+    
+    if(index >=0 && index<[self.dataModel.lists count]){
+        Checklist *checkList=self.dataModel.lists[index];
+        [self performSegueWithIdentifier:@"ShowChecklist" sender:checkList];
+    }
+}
+
+//表视图在显示内容之前先更新它的所有内容
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.dataModel.lists count];
+}
+
+//Asks the data source for a cell to insert in a particular location of the table view.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    Checklist *checklist=self.dataModel.lists[indexPath.row];
+    cell.textLabel.text = checklist.name;
+    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    
+    int remainCount =[checklist countUncheckedItems];
+    int allCount = [checklist.items count];
+    
+    if(allCount == 0){
+        cell.detailTextLabel.text = @"It's empty, add some items.";
+    }else if(allCount >0){
+        if(remainCount == 0){
+            cell.detailTextLabel.text = @"All Done!";
+        }else if (remainCount >0){
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%d Remaining",[checklist countUncheckedItems]];
+        }
+    }
+    
+#pragma mark 注意问题
+    //cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    cell.imageView.image = [UIImage imageNamed:checklist.iconName];
+    
+    return cell;
+}
+
+//将选中的Checklist对象放入sender参数
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.dataModel setIndexOfSelectedChecklist:indexPath.row];
+    Checklist *checklist = self.dataModel.lists[indexPath.row];
+    [self performSegueWithIdentifier:@"ShowChecklist" sender:checklist];
+}
+
+
+//滑动删除记录
+-(void)tableView:(UITableView *)tableView commitEditingStyle: (UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [self.dataModel.lists removeObjectAtIndex:indexPath.row];
+    
+    NSArray *indexPaths = @[indexPath];
+    [tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+-(void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath: (NSIndexPath *)indexPath
+{
+    UINavigationController *navigationController = [self.storyboard instantiateViewControllerWithIdentifier:@"ListNavigationController"];
+    
+    ListDetailViewController *controller = (ListDetailViewController*)navigationController.topViewController;
+    controller.delegate = self;
+    Checklist *checklist = self.dataModel.lists[indexPath.row];
+    controller.checklistToEdit = checklist;
+    
+    [self presentViewController:navigationController animated:YES completion:nil];
+}
+
+#pragma mark segue
+
+//将选中的Checklist对象传递给ChecklistViewController
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if([segue.identifier isEqualToString:@"ShowChecklist"])
+    {
+        ChecklistViewController *controller = segue.destinationViewController;
+        controller.checklist = sender;
+    }else if([segue.identifier isEqualToString:@"AddChecklist"]){
+        UINavigationController *navigationController = segue.destinationViewController;
+        
+        ListDetailViewController *controller=(ListDetailViewController *)navigationController.topViewController;
+
+        controller.delegate=self;
+        controller.checklistToEdit=nil;
+    }
+    
+}
+
+#pragma mark ListDetailViewControllerDelegate
+
+-(void)listDetailViewControllerDidCancel:(ListDetailViewController *)controller{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)listDetailViewController:(ListDetailViewController *)controller didFinishAddingChecklist:(Checklist *)checklist
+{
+    //NSInteger newRowIndex = [self.dataModel.lists count];
+    [self.dataModel.lists addObject:checklist];
+    [self.dataModel sortChecklists];
+    [self.tableView reloadData];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+//    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:newRowIndex inSection:0];
+//    NSArray *indexPaths = @[indexPath];
+//    
+//    [self.tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+-(void)listDetailViewController:(ListDetailViewController *)controller didFinishEditingChecklist:(Checklist *)checklist
+{
+    [self.dataModel sortChecklists];
+    [self.tableView reloadData];
+    
+//    NSInteger index = [self.dataModel.lists indexOfObject:checklist];
+//    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+//    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+//    cell.textLabel.text = checklist.name;
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+#pragma mark UINavigationControllerDelegate
+
+-(void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    if(viewController == self){
+        [self.dataModel setIndexOfSelectedChecklist:-1];
+    }
+}
+
+@end
